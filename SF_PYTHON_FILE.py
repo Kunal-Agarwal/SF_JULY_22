ea6ddd1ec5c2f4b12578f9b10e8618a0de6b1d97
Kunal-Agarwal <33507996+Kunal-Agarwal@users.noreply.github.com>
2022-07-06 15:21:11 +0530

    Update SF_PYTHON_FILE.py

--- SF_PYTHON_FILE.py
+++ SF_PYTHON_FILE.py
@@ -3,12 +3,13 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Butter Dosa')
-streamlit.text('Tawa Fry Vada')
-streamlit.text('Ghee Fried Idli')
+streamlit.text('Butter Dosa 🌶️🌶️🧄🧅')
+streamlit.text('Tawa Fry Vada 🌶️🧄🧅')
+streamlit.text('Ghee Fried Idli 🌶️🧄🧅')
 streamlit.text('Jalebi with Rabdi')
-streamlit.text('Butter Milk')
+streamlit.text('Butter Milk🥤')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
