baa93818aca7e446de32ab16d73189843ff9acb5
Kunal-Agarwal <33507996+Kunal-Agarwal@users.noreply.github.com>
2022-07-06 15:15:20 +0530

    Update SF_PYTHON_FILE.py

--- SF_PYTHON_FILE.py
+++ SF_PYTHON_FILE.py
@@ -1,3 +1,14 @@
 import streamlit
 
-streamlit.title('My better version')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Butter Dosa')
+streamlit.text('Tawa Fry Vada')
+streamlit.text('Ghee Fried Idli')
+streamlit.text('Jalebi with Rabdi')
+streamlit.text('Butter Milk')
+
+
+
+
